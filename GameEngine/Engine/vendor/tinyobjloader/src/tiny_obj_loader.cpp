a9cd4a59ece7e955a62601c0f3c9c4027c830c7a
bagdanchik007 <bogdanskibitskyi@gmail.com>
2026-08-13 00:54:14 +0200

    build(assets): vendor tinyobjloader

---
+++ GameEngine/Engine/vendor/tinyobjloader/src/tiny_obj_loader.cpp
@@ -0,0 +1,9 @@
+// Vendored tinyobjloader (MIT license) from
+// https://github.com/tinyobjloader/tinyobjloader - fetched once and
+// committed, same treatment as stb_image.h and GLAD's generated loader.
+//
+// This is the ONLY translation unit that may define
+// TINYOBJLOADER_IMPLEMENTATION, for the same One-Definition-Rule reason as
+// stb_image.cpp and vendor/glad/src/gl.cpp.
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader/tiny_obj_loader.h>
